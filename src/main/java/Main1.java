3a7fcb1b536867aa9e9317a083e68b8a9a63f09f
Vlad Afonin <followerlsp@gmail.com>
2023-07-05 22:38:17 +0300

    мелкие исправления и добавлен jar файл

---
+++ src/main/java/Main1.java
@@ -0,0 +1,9 @@
+import org.example.Main;
+
+import java.sql.SQLException;
+
+public class Main1 {
+    public static void main(String[] args) throws SQLException {
+        Main.main(args);
+    }
+}
